51fe28bf32d412f39f462ce28ee40e0adb9f49db
Benny1610-yes <benedict.newton10@gmail.com>
2024-02-27 16:29:57 +0000

    COMMENTS in classes Main. code clean up

--- src/Main.java
+++ src/Main.java
@@ -9,121 +9,128 @@ import javax.swing.event.ChangeListener;
 
 public class Main extends JPanel implements ActionListener, ChangeListener{
 
-    ///////////////////////////////////////////////////ALL THE VARIABLES//////////////////////////////////////////////////////////
+   /*
+        What this Class does.
 
-    //EACH OF THE MAZES
+            it does....
+
+
+        extra.
+            ...
+    */
+
+
+
+    /*
+        create 2 2D array sets.
+
+     */
     private final MazeGen my_visited_maze = new MazeGen();
     private MazeGen CurrentMaze  = new MazeGen();
+
+    // create an instance of MazePanel - a JPanel used for mazes to be drawn on for the application.
     private final MazePanel painter = new MazePanel();
+    // create the 'head' of the maze a movable 'character' that will draw the maze,
+    // as it moves around the 'map' (2D array)
+    private final Bloc point = new Bloc(Color.RED);
 
 
-
-    //EAH OF THE pointS FOR EACH FOR CURRENTLY DISPLAYED DEVELOPING MAZES
-    private final Bloc point	= new Bloc(Color.RED);
-
-
-    //EACH OF THE ALGORITHMS TO BE RECALLED FROM THERE CLASS
-    //ONE OF EACH AS WHO WOULD DECIDE TO RUN THE SAME AGAINST EACH OTHER - WHAT KINDA STATE OF MIND WOULD THEY BE IN
+    // Create an instance of the maze generation algorithms to be used for calling and drawing the mazes.
     private final SideWinderAlgorithm sidewinder = new SideWinderAlgorithm();
     private final WilsonAlgorithm wilson = new WilsonAlgorithm();
     private final BinaryTreeAlgorithm binary_tree = new BinaryTreeAlgorithm();
 
-    //NORTH PANEL BUTTONS
+    /*
+        UI for application
 
+            Starting with the North side,
+                size - slider panel that will move to set the size of the desired 'map'
+                reset - button that will be used to reset the 2D array (map) to empty with boarders.
+
+            West side
+                3 buttons - one for each of the algorithms
+                when pressed they will call their corresponding class to start that section of the code
+                to start creating a maze.
+     */
+
+    // north
     private final JSlider size = new JSlider(JSlider.HORIZONTAL,10,100,50);
     private final JButton Reset = new JButton("Blank Canvas");
 
-    //WEST PANEL BUTTONS
+    // west
     private final JButton side_winder_button = new JButton("Sidewinder Algorithm");
     private final JButton binary_tree_button = new JButton("Binary tree Algorithm");
     private final JButton wilson_button = new JButton("Wilson Algorithm");
 
 
 
-    //FUNCTION THAT IS CALLED TO ALL THE MAZES CAN HAVE A VALUE ASSIGNED TO THEM TO MAKE THEIR size
+    // to set the size of the maze whenever the slider moves position to that digit size.
     public void set_maze_size(int xq) {
-
-        //TO MAKE EMPTY MAZES
+        // recreates the maze, as a blank type ready to be used.
         my_visited_maze.emtpy_visited_maze(xq);
     }
 
-    //CONSTRUCTOR
+    //constructor
     public Main() {
 
-        //MUST SET SIZE OF MAZES SO WHEN IT DRAWS EMPTY MAZE TO BEGIN WITH IT DOESN'T CAUSE
-        //A NULL POINTER ERROR
-
+        // set the size of the maze else when maze panel jpanel is called, it will cause null pointer error out of bounds
+        // as there won't be a maze to draw, it'll be an empty 2D array.
         int x = size.getValue();
         set_maze_size(x);
         CurrentMaze.emtpy_visited_maze(x);
+        // call painter, to redraw the maze, using the paint component.
         painter.paint_Maze(CurrentMaze);
 
 
-        //SETTING MAIN CLASS OF EXTENDED JPANEL TO HAVE A BORDER LAYOUT
+        // setting main to have a boarder layout for the corresponding components to be drawn where needed.
         BorderLayout MainLayout = new BorderLayout();
         this.setLayout(MainLayout);
 
-        /////////////////////WEST///////////////////////////////
-
-
-        //WEST JPANEL FOR THE ALGORITHM ACTIVATION BUTTONS
+        /*
+            Grouping the west side buttons in a grid layout,
+            3, 1
+            leaving a column of buttons on the west boarder of the application.
+         */
         JPanel WestPanel = new JPanel();
-
-        //WEST PANEL HAS A GRID LAYOUT OF THE 3 BUTTONS
-        //MAY NEED TO INCREASE IF MORE ALGORITHMS ARE ADDED
         GridLayout WestLayout = new GridLayout(3,1);
-
-        //SETTING THR WEST JPANEL OF BUTTONS A GRIDLAYOUT
         WestPanel.setLayout(WestLayout);
-
+        // adding components to the JPanel
         WestPanel.add(side_winder_button);
         WestPanel.add(binary_tree_button);
         WestPanel.add(wilson_button);
 
 
 
-        /////////////////////NORTH//////////////////////////////
-
-        //NORTHERN JPANEL
+        /*
+            grouping the north side components, in a grid layout.
+            1,2
+            size and reset button, to be drawn on the north border of the Main JPanel.
+         */
         JPanel NorthPanel = new JPanel();
-        //GRID LAYOUT FOR THE NEW PANEL THAT LIES ON TOP
         GridLayout NorthLayout = new GridLayout(1,2);
-
-        //GRID BUTTON AND SLIDER FOR NORTH
         NorthPanel.setLayout(NorthLayout);
-
         NorthPanel.add(size);
         NorthPanel.add(Reset);
 
-        ///////////ACTION LISTENERS/////////////////////////////
+
+
+        // setting action listeners to main.
         side_winder_button.addActionListener(this);
         binary_tree_button.addActionListener(this);
         wilson_button.addActionListener(this);
         Reset.addActionListener(this);
 
-        //do all the fancy JSlider stuff.
+        // J slider settings, such as length, spacing, how it needs to be drawn basically.
         size.setMajorTickSpacing(10);
         size.setMinorTickSpacing(5);
         size.setPaintTicks(true);
         size.setPaintLabels(true);
         size.addChangeListener(this);
 
-        //adding all the panels to main display
+        //adding earlier panels, to the main panel. in earlier stated grid layout.
         this.add(NorthPanel, BorderLayout.NORTH);
         this.add(WestPanel, BorderLayout.WEST);
         this.add(painter, BorderLayout.CENTER);
-    }
-
-
-
-
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////MAIN PAINT COMPONENT//////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    public void paintComponent(Graphics g)
-    {
-        super.paintComponent(g);
-
     }
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -131,6 +138,10 @@ public class Main extends JPanel implements ActionListener, ChangeListener{
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+    /*
+        action events, for capturing, whenever anything is pressed on the application, to allow for them to have logic,
+        else a button could be clicked and would do anything as the action taken wouldn't be recorded and sent here.
+     */
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == side_winder_button) {
             CurrentMaze = (sidewinder.sidewinder(my_visited_maze));
@@ -152,6 +163,7 @@ public class Main extends JPanel implements ActionListener, ChangeListener{
         repaint();
     }
 
+    // to capture the slider, as it changes state, it doesn't just turn on and off like a button.
     @Override
     public void stateChanged(ChangeEvent e) {
         int x = size.getValue();
